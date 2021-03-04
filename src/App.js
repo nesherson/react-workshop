15bf9e312ae9fd9d2610fd5f77b10f106c4d3f4b
unknown <nesad.omeragich27@gmail.com>
2021-03-04 23:20:46 +0100

    Added basic project structure for code academy authorization exercise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
-import { animals } from './animals';
+import React from 'react';
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'swordfish',
+      authorized: false,
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector('input[type="password"]').value;
+    const auth = password === this.state.password;
+    this.setState({
+      authorized: auth,
+    });
+  }
+
+  render() {
+    return (
+      <div id='authorization'>
+        <h1>Contact</h1>
+        <ul>
+          <li>client@example.com</li>
+          <li>555.555.5555</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className='App'>
-      <h1>{animals.dolphin.facts[0]}</h1>
-    </div>
-  );
+  return <Contact />;
 }
 
 export default App;
