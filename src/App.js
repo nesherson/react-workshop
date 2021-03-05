bb93ce585d3c1550ea0575f1b266c7f90b7a2bd3
unknown <nesad.omeragich27@gmail.com>
2021-03-05 23:49:02 +0100

    created project structure for second codeacademy exercise

--- src/App.js
+++ src/App.js
@@ -1,59 +1,47 @@
 import './App.css';
 import React from 'react';
 
-class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      password: 'swordfish',
-      authorized: false,
-    };
-    this.authorize = this.authorize.bind(this);
+class Random extends React.Component {
+  componentDidMount() {
+    this.applyColor();
   }
 
-  authorize(e) {
-    const password = e.target.querySelector('input[type="password"]').value;
-    const auth = password === this.state.password;
-    this.setState({
-      authorized: auth,
-    });
+  componentDidUpdate(prevProps, prevState) {
+    this.applyColor();
+  }
+
+  formatColor(ary) {
+    return 'rgb(' + ary.join(', ') + ')';
+  }
+
+  isLight() {
+    const rgb = this.state.color;
+    return rgb.reduce((a, b) => a + b) < 127 * 3;
+  }
+
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.background = color;
+  }
+
+  chooseColor() {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random() * 256));
+    }
+    return random;
   }
 
   render() {
-    const login = (
-      <form action='#' onSubmit={this.authorize}>
-        <input type='password' placeholder='Password' />
-        <input type='Submit' />
-      </form>
-    );
-
-    const contactInfo = (
-      <ul>
-        <li>client@example.com</li>
-        <li>555.555.5555</li>
-      </ul>
-    );
-
     return (
-      <div id='authorization'>
-        {this.state.authorized ? (
-          <div>
-            <h1>Contact</h1>
-            {contactInfo}
-          </div>
-        ) : (
-          <div>
-            <h1>Enter your password</h1>
-            {login}
-          </div>
-        )}
+      <div>
+        <h1 className={this.isLight() ? 'white' : 'black'}></h1>
       </div>
     );
   }
 }
-
 function App() {
-  return <Contact />;
+  return <Random />;
 }
 
 export default App;
