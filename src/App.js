067022cefeb4457f4a4284053166250e6dea0b52
unknown <nesad.omeragich27@gmail.com>
2021-03-06 23:42:49 +0100

    Removed older project files and started new CA exercise

--- src/App.js
+++ src/App.js
@@ -1,65 +1,63 @@
 import './App.css';
 import React from 'react';
-import { Button } from './Button';
+import { Profile } from './Profile';
+import { Directory } from './Directory';
 
-class Random extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: [0, 0, 0],
+      currentUsername: null,
     };
-    this.handleClick = this.handleClick.bind(this);
+    this.handleChoose = this.handleChoose.bind(this);
+    this.handleReturnToDirectoryClick = this.handleReturnToDirectoryClick.bind(
+      this
+    );
   }
 
-  componentDidMount() {
-    this.applyColor();
+  handleChoose(newUsername) {
+    this.setState({ currentUsername: newUsername });
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    this.applyColor();
-  }
-
-  formatColor(ary) {
-    return 'rgb(' + ary.join(', ') + ')';
-  }
-
-  isLight() {
-    const rgb = this.state.color;
-    return rgb.reduce((a, b) => a + b) < 127 * 3;
-  }
-
-  applyColor() {
-    const color = this.formatColor(this.state.color);
-    document.body.style.background = color;
-  }
-
-  chooseColor() {
-    const random = [];
-    for (let i = 0; i < 3; i++) {
-      random.push(Math.floor(Math.random() * 256));
-    }
-    return random;
-  }
-
-  handleClick() {
-    this.setState({
-      color: this.chooseColor(),
-    });
+  handleReturnToDirectoryClick() {
+    this.setState({ currentUsername: null });
   }
 
   render() {
+    let body;
+    if (this.state.currentUsername) {
+      body = (
+        <Profile
+          username={this.state.currentUsername}
+          onChoose={this.handleChoose}
+        />
+      );
+    } else {
+      body = <Directory onChoose={this.handleChoose} />;
+    }
+
     return (
-      <div>
-        <h1 className={this.isLight() ? 'white' : 'black'}>
-          This color is {this.formatColor(this.state.color)}
-        </h1>
-        <Button light={this.isLight} onClick={this.handleClick} />
+      <div className='App'>
+        <header>
+          <h1>PetBook</h1>
+
+          <nav>
+            {this.state.currentUsername && (
+              <button onClick={this.handleReturnToDirectoryClick}>
+                Return to directory
+              </button>
+            )}
+          </nav>
+        </header>
+
+        <main>{body}</main>
       </div>
     );
   }
 }
+
 function App() {
-  return <Random />;
+  return <App />;
 }
 
 export default App;
