778a69eb384a1e3c112bce07881d26e0fe94e934
unknown <nesad.omeragich27@gmail.com>
2021-03-08 23:59:40 +0100

    Changed class components to functional components

--- src/App.js
+++ src/App.js
@@ -1,64 +1,35 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import NewTask from './NewTask';
 import TasksList from './TasksList';
 
-class AppClass extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newTask: {},
-      allTasks: [],
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-  }
-
-  handleChange({ target }) {
+export default function AppFunction() {
+  const [newTask, setNewTask] = useState({});
+  const handleChange = ({ target }) => {
     const { name, value } = target;
-    this.setState((prevState) => ({
-      ...prevState,
-      newTask: {
-        ...prevState.newTask,
-        [name]: value,
-        id: Date.now(),
-      },
-    }));
-  }
+    setNewTask((prev) => ({ ...prev, id: Date.now(), [name]: value }));
+  };
 
-  handleSubmit(event) {
+  const [allTasks, setAllTasks] = useState([]);
+  const handleSubmit = (event) => {
     event.preventDefault();
-    if (!this.state.newTask.title) return;
-    this.setState((prevState) => ({
-      allTasks: [prevState.newTask, ...prevState.allTasks],
-      newTask: {},
-    }));
-  }
+    if (!newTask.title) return;
+    setAllTasks((prev) => [newTask, ...prev]);
+    setNewTask({});
+  };
+  const handleDelete = (taskIdToRemove) => {
+    setAllTasks((prev) => prev.filter((task) => task.id !== taskIdToRemove));
+  };
 
-  handleDelete(taskIdToRemove) {
-    this.setState((prevState) => ({
-      ...prevState,
-      allTasks: prevState.allTasks.filter((task) => task.id !== taskIdToRemove),
-    }));
-  }
-
-  render() {
-    return (
-      <main>
-        <h1>Tasks</h1>
-        <NewTask
-          newTask={this.state.newTask}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-        />
-        <TasksList
-          allTasks={this.state.allTasks}
-          handleDelete={this.handleDelete}
-        />
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h1>Tasks</h1>
+      <NewTask
+        newTask={newTask}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <TasksList allTasks={allTasks} handleDelete={handleDelete} />
+    </main>
+  );
 }
-
-export default AppClass;
