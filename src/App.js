8dd47ee7192a1bea87c8b359ec6b58b29ecd8109
unknown <nesad.omeragich27@gmail.com>
2021-03-10 00:23:22 +0100

    Added separate hooks for fetching data and aditional code refactoring

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
+import './App.css';
 import React, { useState, useEffect } from 'react';
 import { get } from './mockBackend/fetch';
 
 export default function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
+  /*useEffect(() => {
     Promise.all([get('/menu'), get('/news-feed'), get('/friends')]).then(
       ([menuResponse, newsFeedResponse, friendsResponse]) => {
-        setData({
-          menu: menuResponse.data,
-          newsFeed: newsFeedResponse.data,
-          friends: friendsResponse.data,
-        });
+        setMenu(menuResponse.data);
+        setNewsFeed(newsFeedResponse.data);
+        setFriends(friendsResponse.data);
       }
     );
+  }, []);
+*/
+  const [menu, setMenu] = useState(null);
+  useEffect(() => {
+    get('/menu').then((menuResponse) => {
+      setMenu(menuResponse.data);
+    });
+  }, []);
+
+  const [newsFeed, setNewsFeed] = useState(null);
+  useEffect(() => {
+    get('/news-feed').then((newsFeedResponse) => {
+      setNewsFeed(newsFeedResponse.data);
+    });
+  }, []);
+
+  const [friends, setFriends] = useState(null);
+  useEffect(() => {
+    get('/friends').then((friendsResponse) => {
+      setFriends(friendsResponse.data);
+    });
   }, []);
 
   return (
     <div className='App'>
       <h1>My Network</h1>
-      {!data || !data.menu ? (
+      {!menu ? (
         <p>Loading..</p>
       ) : (
         <nav>
-          {data.menu.map((menuItem) => (
+          {menu.map((menuItem) => (
             <button key={menuItem}>{menuItem}</button>
           ))}
         </nav>
       )}
       <div className='content'>
-        {!data || !data.newsFeed ? (
+        {!newsFeed ? (
           <p>Loading..</p>
         ) : (
           <section>
-            {data.newsFeed.map(({ id, title, message, imgSrc }) => (
+            {newsFeed.map(({ id, title, message, imgSrc }) => (
               <article key={id}>
                 <h3>{title}</h3>
                 <p>{message}</p>
@@ -42,12 +60,12 @@ export default function App() {
             ))}
           </section>
         )}
-        {!data || !data.friends ? (
+        {!friends ? (
           <p>Loading..</p>
         ) : (
           <aside>
             <ul>
-              {data.friends
+              {friends
                 .sort((a, b) => (a.isOnline && !b.isOnline ? -1 : 0))
                 .map(({ id, name, isOnline }) => (
                   <li key={id} className={isOnline ? 'online' : 'offline'}>
