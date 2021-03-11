46bd9f19ac7b0ad67ba07e16dccccace4e63aa28
unknown <nesad.omeragich27@gmail.com>
2021-03-11 23:48:19 +0100

    Set up basic project structure for next CA project

--- src/App.js
+++ src/App.js
@@ -1,89 +1,29 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import { AddThoughtForm } from './AddThoughtForm';
-import { Thought } from './Thought';
-import { generateId, getNewExpirationTime } from './utilities';
+import React, { useState } from 'react';
 
-function App() {
-  const [newThought, setNewThought] = useState('');
-  const [thoughts, setThoughts] = useState([
-    {
-      id: generateId(),
-      text: 'This is a place for your passing thoughts.',
-      expiresAt: getNewExpirationTime(),
-    },
-    {
-      id: generateId(),
-      text: "They'll be removed after 15 seconds.",
-      expiresAt: getNewExpirationTime(),
-    },
-  ]);
+const VIDEOS = {
+  fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
+  slow: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
+  cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
+  eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4',
+};
 
-  const areDatesEqual = (date1_value, date2_value) => {
-    return new Date(date1_value).getSeconds() ===
-      new Date(date2_value).getSeconds()
-      ? true
-      : false;
-  };
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  useEffect(() => {
-    if (thoughts) {
-      const intervalId = setInterval(() => {
-        setThoughts((prev) =>
-          prev.filter((t) => !areDatesEqual(t.expiresAt, Date.now()))
-        );
-      }, 1000);
-      return () => clearInterval(intervalId);
-    }
-  }, []);
+    this.state = { src: VIDEOS.fast };
+  }
 
-  const onChange = ({ target }) => {
-    setNewThought(target.value);
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    setThoughts((prev) => {
-      return [
-        ...prev,
-        {
-          id: generateId(),
-          text: newThought,
-          expiresAt: getNewExpirationTime(),
-        },
-      ];
-    });
-    setNewThought('');
-  };
-
-  const handleRemoveThought = (id) => {
-    setThoughts((prev) => prev.filter((t) => t.id !== id));
-    console.log('handle');
-  };
-
-  return (
-    <div className='App'>
-      <header>
-        <h1>Passing Thoughts</h1>
-      </header>
-      <main>
-        <AddThoughtForm
-          onChange={onChange}
-          value={newThought}
-          onSubmit={onSubmit}
-        />
-        <ul className='thoughts'>
-          {thoughts.map((thought) => (
-            <Thought
-              key={thought.id}
-              thought={thought}
-              removeThought={handleRemoveThought}
-            />
-          ))}
-        </ul>
-      </main>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>Video Player</h1>
+        <Menu />
+        <Video />
+      </div>
+    );
+  }
 }
 
 export default App;
