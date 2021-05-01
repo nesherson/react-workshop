91d3012d9783bf8d5ecb8dd54cfbcd2e173108d9
nesherson <nesad.omeragich27@gmail.com>
2021-05-02 01:40:48 +0200

    Added state for dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './index.css';
+import { useState } from 'react'; 
+
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
 
 const App = () => {
+
+	const [open, setOpen] = useState();
+
 	return (
 		<Navbar>
-			<NavItem icon='1'/>
-			<NavItem icon='2'/>
-			<NavItem icon='3'/>
+			<NavItem icon={<PlusIcon />}/>
+			<NavItem icon={<BellIcon />}/>
+			<NavItem icon={<MessengerIcon />}/>
+			<NavItem icon={<CaretIcon/>}>
+			</NavItem>
 		</Navbar>
 	);
 };
