3671e528190be0912fccc79123939690dd2dafef
unknown <nesad.omeragich27@gmail.com>
2021-05-18 03:27:53 +0200

    Initial project structure for react exercise

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
