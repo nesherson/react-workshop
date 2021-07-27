3f6f9bc1d1d040fa488098d170004dc318a41d67
nesherson <nesad.omeragich27@gmail.com>
2021-07-27 02:55:03 +0200

    Add todo reducers and create redux store

--- src/redux.ts
+++ src/redux.ts
@@ -1,85 +1,209 @@
-import { v1 as uuid } from 'uuid'
+import { v1 as uuid } from "uuid";
+import { combineReducers, createStore } from "redux";
 
-import { Todo } from './type'
+import { Todo } from "./type";
 
-const CREATE_TODO = 'CREATE_TODO'
-const EDIT_TODO = 'EDIT_TODO'
-const TOGGLE_TODO = 'TOGGLE_TODO'
-const DELETE_TODO = 'DELETE_TODO'
-const SELECT_TODO = 'SELECT_TODO'
+const CREATE_TODO = "CREATE_TODO";
+const EDIT_TODO = "EDIT_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const DELETE_TODO = "DELETE_TODO";
+const SELECT_TODO = "SELECT_TODO";
 
 interface CreateTodoActionType {
-    type: typeof CREATE_TODO
-    payload: Todo
+  type: typeof CREATE_TODO;
+  payload: Todo;
 }
 
-export const createTodoActionCreator = ({description}: {
-    description: string
+export const createTodoActionCreator = ({
+  description,
+}: {
+  description: string;
 }): CreateTodoActionType => {
-    return {
-        type: CREATE_TODO,
-        payload: {
-            id: uuid(),
-            description,
-            isComplete: false
-        }
-    }
-}
+  return {
+    type: CREATE_TODO,
+    payload: {
+      id: uuid(),
+      description,
+      isComplete: false,
+    },
+  };
+};
 
 interface EditTodoActionType {
-    type: typeof EDIT_TODO
-    payload: { id: string, description: string}
+  type: typeof EDIT_TODO;
+  payload: { id: string; description: string };
 }
 
-export const editTodoActionCreator = ({id, description}: { id: string, description: string}): EditTodoActionType => {
-    return {
-        type: EDIT_TODO,
-        payload: {
-            id,
-            description
-        }
-    }
-}
+export const editTodoActionCreator = ({
+  id,
+  description,
+}: {
+  id: string;
+  description: string;
+}): EditTodoActionType => {
+  return {
+    type: EDIT_TODO,
+    payload: {
+      id,
+      description,
+    },
+  };
+};
 
 interface ToggleTodoActionType {
-    type: typeof TOGGLE_TODO,
-    payload: { id: string, isComplete: boolean}
+  type: typeof TOGGLE_TODO;
+  payload: { id: string; isComplete: boolean };
 }
 
-export const toggleTodoActionCreator = ({id, isComplete}: {id: string, isComplete: boolean}): ToggleTodoActionType => {
-    return {
-        type: TOGGLE_TODO,
-        payload: {
-            id,
-            isComplete
-        }
-    }
-}
+export const toggleTodoActionCreator = ({
+  id,
+  isComplete,
+}: {
+  id: string;
+  isComplete: boolean;
+}): ToggleTodoActionType => {
+  return {
+    type: TOGGLE_TODO,
+    payload: {
+      id,
+      isComplete,
+    },
+  };
+};
 
 interface DeleteTodoActionType {
-    type: typeof DELETE_TODO,
-    payload: {id: string}
+  type: typeof DELETE_TODO;
+  payload: { id: string };
 }
 
-export const deleteTodoActionCreator = ({id}: {id: string}): DeleteTodoActionType => {
-    return {
-        type: DELETE_TODO,
-        payload: {
-            id
-        }
-    }
-}
+export const deleteTodoActionCreator = ({
+  id,
+}: {
+  id: string;
+}): DeleteTodoActionType => {
+  return {
+    type: DELETE_TODO,
+    payload: {
+      id,
+    },
+  };
+};
 
 interface SelectTodoActionType {
-    type: typeof SELECT_TODO,
-    payload: {id: string}
+  type: typeof SELECT_TODO;
+  payload: { id: string };
 }
 
-export const selectTodoActionCreator = ({id}: {id: string}): SelectTodoActionType => {
-    return {
-        type: SELECT_TODO,
-        payload: {
-            id
-        }
+export const selectTodoActionCreator = ({
+  id,
+}: {
+  id: string;
+}): SelectTodoActionType => {
+  return {
+    type: SELECT_TODO,
+    payload: {
+      id,
+    },
+  };
+};
+
+const todosInitialState: Todo[] = [
+  {
+    id: uuid(),
+    description: "Learn React",
+    isComplete: true,
+  },
+  {
+    id: uuid(),
+    description: "Learn Redux",
+    isComplete: true,
+  },
+  {
+    id: uuid(),
+    description: "Learn Redux-ToolKit",
+    isComplete: false,
+  },
+];
+
+type TodoActionTypes =
+  | CreateTodoActionType
+  | EditTodoActionType
+  | ToggleTodoActionType
+  | DeleteTodoActionType;
+
+const todosReducer = (
+  state: Todo[] = todosInitialState,
+  action: TodoActionTypes
+) => {
+  switch (action.type) {
+    case CREATE_TODO: {
+      const { payload } = action;
+
+      return [...state, payload];
     }
-}
+    case EDIT_TODO: {
+      const { payload } = action;
+
+      return state.map((todo) =>
+        todo.id === payload.id
+          ? { ...todo, description: payload.description }
+          : todo
+      );
+    }
+    case TOGGLE_TODO: {
+      const { payload } = action;
+
+      return state.map((todo) =>
+        todo.id === payload.id
+          ? { ...todo, isComplete: payload.isComplete }
+          : todo
+      );
+    }
+    case DELETE_TODO: {
+      const { payload } = action;
+
+      return state.filter((todo) => todo.id !== payload.id);
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+type SelectedTodoActionTypes = SelectTodoActionType;
+const selectedTodoReducer = (
+  state: string | null = null,
+  action: SelectedTodoActionTypes
+) => {
+  switch (action.type) {
+    case SELECT_TODO: {
+      const { payload } = action;
+      return payload.id;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+const counterReducer = (state: number = 0, action: TodoActionTypes) => {
+  switch (action.type) {
+    case CREATE_TODO:
+    case EDIT_TODO:
+    case TOGGLE_TODO:
+    case DELETE_TODO: {
+      return state + 1;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+const reducers = combineReducers({
+    todos: todosReducer,
+    selectedTodo: selectedTodoReducer,
+    counter: counterReducer
+});
+
+export default createStore(reducers);
