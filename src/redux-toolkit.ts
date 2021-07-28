5213c4394b81ab895c6ae8ce0d44fad165deb2e0
nesherson <nesad.omeragich27@gmail.com>
2021-07-28 22:59:49 +0200

    Add selected todo slice

--- src/redux-toolkit.ts
+++ src/redux-toolkit.ts
@@ -60,3 +60,14 @@ const todosSlice = createSlice({
     }
 
 })
+
+const selectedTodoSlice = createSlice({
+    name: 'selectedTodo',
+    initialState: null as string | null,
+    reducers: {
+        select: (state, {payload}: PayloadAction<{id: string}>) => {
+            return payload.id
+        }
+    }
+})
+
